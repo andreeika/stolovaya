00da165c13b4b19a5f55936fda617a1fdd7b76ad
Андрей Сенько <andrey_senko_06@vk.com>
2025-01-30 19:24:21 +0700

    Норма кофе выпита, добавлены окна приложения

--- app/src/main/java/com/example/stolovaya/MainActivity.kt
+++ app/src/main/java/com/example/stolovaya/MainActivity.kt
@@ -1,59 +1,94 @@
 package com.example.stolovaya
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import com.example.stolovaya.databinding.ActivityMainBinding
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var textView_salades: TextView
+    private lateinit var textView_soup: TextView
+    private lateinit var textView_myaso: TextView
+    private lateinit var textView_garnir: TextView
+    private lateinit var textView_bulochki: TextView
+    private lateinit var textView_deserts: TextView
+    private lateinit var textView_napitki: TextView
+    private lateinit var button_korzina: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+        //Цвет для строки состояния
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.my_status_bar_color)
         }
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        //Цвет для нижней строки с кнопками домой
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.navigationBarColor = ContextCompat.getColor(this, R.color.my_status_bar_color)
         }
+
+        // Инициализация View
+        textView_salades = findViewById(R.id.textView)
+        textView_soup = findViewById(R.id.textView3)
+        textView_myaso = findViewById(R.id.textView4)
+        textView_garnir = findViewById(R.id.textView5)
+        textView_bulochki = findViewById(R.id.textView6)
+        textView_deserts = findViewById(R.id.textView9)
+        textView_napitki = findViewById(R.id.textView7)
+        button_korzina = findViewById(R.id.button)
+
+        // Обработчик нажатия на кнопку и переход на окно
+
+        button_korzina.setOnClickListener {
+            val intent = Intent(this@MainActivity, MainActivity_Korzina::class.java)
+            startActivity(intent)
+        }
+
+        textView_salades.setOnClickListener {
+            val intent = Intent(this@MainActivity, MainActivity_Salades::class.java)
+            startActivity(intent)
+        }
+
+        textView_myaso.setOnClickListener {
+            val intent = Intent(this@MainActivity, MainActivity_Myaso::class.java)
+            startActivity(intent)
+        }
+
+        textView_garnir.setOnClickListener {
+            val intent = Intent(this@MainActivity, MainActivity_Garnir::class.java)
+            startActivity(intent)
+        }
+
+
+        textView_soup.setOnClickListener {
+            val intent = Intent(this@MainActivity, MainActivity_Soup::class.java)
+            startActivity(intent)
+        }
+
+        textView_bulochki.setOnClickListener {
+            val intent = Intent(this@MainActivity, MainActivity_Bulochki::class.java)
+            startActivity(intent)
+        }
+
+        textView_deserts.setOnClickListener {
+            val intent = Intent(this@MainActivity, MainActivity_Deserts::class.java)
+            startActivity(intent)
+        }
+
+        textView_napitki.setOnClickListener {
+            val intent = Intent(this@MainActivity, MainActivity_Napitki::class.java)
+            startActivity(intent)
+        }
+
+
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
---
+++ app/src/main/java/com/example/stolovaya/MainActivity_Napitki.kt
@@ -0,0 +1,22 @@
+package com.example.stolovaya
+
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity_Napitki : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_napitki)
+
+        //Цвет для нижней строки с кнопками домой
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.navigationBarColor = ContextCompat.getColor(this, R.color.my_status_bar_color)
+        }
+    }
+}
