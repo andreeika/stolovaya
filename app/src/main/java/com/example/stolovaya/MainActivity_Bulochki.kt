b60a5ab28f88dcc561a5ac79701fcea74e05a418
Александр Данилов <23san00@gmail.com>
2025-02-02 11:51:24 +0700

    закруглил все кнопки кроме корзины (не представляю на кой нам там кнопка Корзина), сделал переходы в корзину из вкладок, все работает, а у меня там вскипятился чайник, сейчас пойду кофе пить 💅☕️

--- app/src/main/java/com/example/stolovaya/MainActivity_Bulochki.kt
+++ app/src/main/java/com/example/stolovaya/MainActivity_Bulochki.kt
@@ -1,7 +1,9 @@
 package com.example.stolovaya
 
+import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -9,14 +11,29 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity_Bulochki : AppCompatActivity() {
+
+    private lateinit var button_korzina: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main_bulochki)
 
+        //Цвет для строки состояния
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.my_status_bar_color)
+        }
+
         //Цвет для нижней строки с кнопками домой
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             window.navigationBarColor = ContextCompat.getColor(this, R.color.my_status_bar_color)
+        }
+
+        button_korzina = findViewById(R.id.button)
+
+        button_korzina.setOnClickListener {
+            val intent = Intent(this@MainActivity_Bulochki, MainActivity_Korzina::class.java)
+            startActivity(intent)
         }
     }
 }
