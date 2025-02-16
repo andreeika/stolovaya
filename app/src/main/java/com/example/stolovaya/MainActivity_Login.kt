9990ab686cef308f58b8b0ded8c6eb059d7d98db
banil <banil200545@gmail.com>
2025-02-16 18:51:31 +0700

    добавил возврат из логина в главное меню

--- app/src/main/java/com/example/stolovaya/MainActivity_Login.kt
+++ app/src/main/java/com/example/stolovaya/MainActivity_Login.kt
@@ -8,7 +8,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity_Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
