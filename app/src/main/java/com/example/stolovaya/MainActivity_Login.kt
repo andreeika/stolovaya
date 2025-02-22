97713a10b010f7120150d905afbacbf42915b07f
Андрей Сенько <andrey_senko_06@vk.com>
2025-02-22 16:31:31 +0700

    добавлено окно авторизации

--- app/src/main/java/com/example/stolovaya/MainActivity_Login.kt
+++ app/src/main/java/com/example/stolovaya/MainActivity_Login.kt
@@ -3,13 +3,23 @@ package com.example.stolovaya
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 
 class MainActivity_Login : AppCompatActivity() {
+    private lateinit var editTextUsername: EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var buttonLogin: Button
+
+    private val correctUsername = "user"
+    private val correctPassword = "password"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -26,11 +36,34 @@ class MainActivity_Login : AppCompatActivity() {
         }
 
         lateinit var logo: ImageView
-
         logo = findViewById(R.id.imageView4)
         logo.setOnClickListener{
             val intent = Intent(this@MainActivity_Login, MainActivity::class.java)
             startActivity(intent)
         }
+
+        editTextUsername = findViewById(R.id.editTextUsername)
+        editTextPassword = findViewById(R.id.editTextPassword)
+        buttonLogin = findViewById(R.id.button)
+
+
+        buttonLogin.setOnClickListener {
+            val username = editTextUsername.text.toString()
+            val password = editTextPassword.text.toString()
+
+            // Проверка авторизации
+            if (username == correctUsername && password == correctPassword) {
+                // Авторизация успешна
+                Toast.makeText(this, "Авторизация успешна!", Toast.LENGTH_SHORT).show()
+
+                    val intent = Intent(this@MainActivity_Login, MainActivity_OknoPersonala::class.java)
+                    startActivity(intent)
+
+            } else {
+                // Авторизация не удалась
+                Toast.makeText(this, "Неверный логин или пароль!", Toast.LENGTH_SHORT).show()
+            }
+
+
     }
-}
+}}
