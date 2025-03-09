e35d0695545f146f7e179cdfba4e9edcdd0badc4
Андрей Сенько <andrey_senko_06@vk.com>
2025-03-09 19:12:43 +0700

    перенос выбранных блюд в корзину

--- app/src/main/java/com/example/stolovaya/CustomAdapter.kt
+++ app/src/main/java/com/example/stolovaya/CustomAdapter.kt
@@ -4,18 +4,25 @@ import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-data class ItemsViewModel(val image: Bitmap?, val text: String, val image2: Bitmap?, val text2: String)
-class CustomAdapter(private val mList: List<ItemsViewModel>) :
+data class ItemsViewModel(val image: Bitmap?, val text: String)
+class CustomAdapter(private val mList: List<ItemsViewModel>, private val listener: OnItemClickListener) :
     RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+    // Интерфейс для обработки нажатий на кнопку
+    interface OnItemClickListener {
+        fun onKorzinaClick(item: ItemsViewModel)
+    }
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.card_view_design, parent, false)
+            .inflate(R.layout.card_view_design_salades, parent, false)
 
         return ViewHolder(view)
     }
@@ -24,20 +31,19 @@ class CustomAdapter(private val mList: List<ItemsViewModel>) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         val ItemsViewModel = mList[position]
-
-        // Установка данных для первой картинки и подписи
-        holder.imageView.setImageBitmap(ItemsViewModel.image)
+        holder.btn.setOnClickListener {
+            listener.onKorzinaClick(ItemsViewModel)// при нажатии на кнопку "+" интерфейс переносит информацию
+                                                   // о конкретном блюде в корзину
+        }
         holder.textView.text = ItemsViewModel.text
+        // Установка картинки
+        ItemsViewModel.image?.let {
+            holder.imageView.setImageBitmap(it)
 
-        // Установка данных для второй картинки и подписи
-        holder.imageView3.setImageBitmap(ItemsViewModel.image2)
-        holder.textView14.text = ItemsViewModel.text2
+        } ?: run {
 
-
-
-
-
-
+        }
+        holder.imageView.setImageBitmap(ItemsViewModel.image)
     }
 
 
@@ -46,15 +52,10 @@ class CustomAdapter(private val mList: List<ItemsViewModel>) :
     }
 
 
-
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
         val imageView: ImageView = itemView.findViewById(R.id.imageview)
         val textView: TextView = itemView.findViewById(R.id.textView)
-        val imageView3: ImageView = itemView.findViewById(R.id.imageview3)
-        val textView14: TextView = itemView.findViewById(R.id.textView14)
-
-
-
+        val btn: Button = itemView.findViewById(R.id.btnKorzina)
     }
 
 
